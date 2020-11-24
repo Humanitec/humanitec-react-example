480cd91bf544eaa12ac76f6d53ba88d9d89b997f
Aziz <aziz@humanitec.com>
2020-11-24 18:33:52 +0100

    basic styling

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 window.env = {
   API_URL: 'http://api.example.com',
-  API_URL2: 'http://api.example.com',
-  API_URL3: 'http://api.example.com',
+  WEBSOCKET_URL: 'wss://api.example.com',
+  ENVIRONMENT_NAME: 'local',
 };
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ const AppWrapper = styled.div`
   font-family: Lexend Deca;
 `;
 
+const EnvVariablesList = styled.ul`
+  width: 50%;
+  padding: 0;
+`;
+
+const EnvVariable = styled.li`
+  font-size: 14px;
+  background-color: aliceblue;
+  border: 1px solid cornflowerblue;
+  border-radius: 5px;
+  margin: 10px 0;
+  padding: 10px 8px;
+  list-style: none;
+`;
+
 const EnvVariableValue = styled.span`
   font-size: 14px;
 `;
@@ -14,15 +29,15 @@ const App = () => {
   return (
     <AppWrapper>
       <h2> Environment Variables </h2>
-      <ul>
+      <EnvVariablesList>
         {Object.entries(windowEnv).map(([key, value]: any) => {
           return (
-            <li key={key}>
-              {key} : <EnvVariableValue>{value}</EnvVariableValue>
-            </li>
+            <EnvVariable key={key}>
+              {key}: <EnvVariableValue>{value}</EnvVariableValue>
+            </EnvVariable>
           );
         })}
-      </ul>
+      </EnvVariablesList>
     </AppWrapper>
   );
 };

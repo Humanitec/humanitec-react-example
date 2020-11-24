17807bf2a6bab2d55fd641bad052eacad045556d
Aziz <aziz@humanitec.com>
2020-11-24 16:11:08 +0100

    add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { windowEnv } from '@src/environment';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  font-family: Lexend Deca;
+`;
+
+const EnvVariableValue = styled.span`
+  font-size: 14px;
+`;
 
 const App = () => {
-  return <div>{windowEnv.API_URL}</div>;
+  return (
+    <AppWrapper>
+      <h2> Environment Variables </h2>
+      <ul>
+        <li>
+          API_URL: <EnvVariableValue>{windowEnv.API_URL}</EnvVariableValue>
+        </li>
+      </ul>
+    </AppWrapper>
+  );
 };
 
 export default App;
